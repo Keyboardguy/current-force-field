c43e83333620c2442441c6946c0e0c4bef8f5379
George <george200579@gmail.com>
2024-01-20 18:24:03 +0000

    add existing project files to github

---
+++ src/app/calculations.tsx
@@ -0,0 +1,17 @@
+interface vector3d {
+    x: number;
+    y: number;
+    z: number;
+}
+
+function cross_
+
+
+
+
+function test_function(): void {
+    console.log("Works");
+    return;
+}
+
+export default test_function;
