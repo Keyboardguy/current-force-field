29c438e09d20ca2732ed2b69be3bee9e4b0353de
George <george200579@gmail.com>
2024-01-21 23:48:03 +0000

    front end is fine now

--- src/app/canvas.tsx
+++ src/app/canvas.tsx
@@ -1,9 +1,80 @@
 import { useRef, useEffect } from 'react';
-import styles from '../../styles/page.module.css';
+import styles from './page.module.css';
 import { direction, charge } from './helper';
 
 type directionOrNull = direction | null;
 
+function draw_diagram(canvas:HTMLCanvasElement, field: directionOrNull, current: directionOrNull, force: directionOrNull, charge: charge | null) {
+  function draw_arrow(fromx: number, fromy: number, tox: number, toy: number) {
+    ctx.beginPath();
+    const headlen = 8; // length of head in pixels
+    const dx = tox - fromx;
+    const dy = toy - fromy;
+    const angle = Math.atan2(dy, dx);
+    ctx.moveTo(fromx, fromy);
+    ctx.lineTo(tox, toy);
+    ctx.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+    ctx.moveTo(tox, toy);
+    ctx.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+    ctx.stroke();
+  }
+
+  function draw_field() {
+    function draw_circles
+    switch (field) {
+      case "left":
+        console.log("unimplemenmted");
+        break;
+      case "right":
+        console.log("unimplemenmted");
+        break;
+      case "up":
+        console.log("unimplemenmted");
+        break;
+      case "down":
+        console.log("unimplemenmted");
+        break;
+      case "in the page":
+        console.log("unimplemenmted");
+        break;
+      case "out the page":
+        console.log("unimplemenmted");
+        break;
+    }
+
+    return;
+  }
+
+  function draw_current() {
+
+  }
+
+  function draw_force() {
+
+  }
+
+  function draw_particle() {
+
+  }
+
+  // ive asserted it's not null, but i still check for it. It's just to get rid of the errors above.
+	const ctx = canvas.getContext("2d")!;
+
+  if (!ctx) {
+    throw new Error("The canvas doesn't exist");
+  }
+
+
+  const centre: [number, number] = [canvas.width / 2, canvas.height / 2];
+
+	ctx.font = "20px Helvetica";
+	ctx.textBaseline = "middle";
+	ctx.textAlign = "center";
+	ctx.fillStyle = "#FFFFFF";
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "#000000";
+    
+}
 
 export default function Canvas({
   field,
@@ -16,16 +87,17 @@ export default function Canvas({
   force: directionOrNull,
   charge: charge | null
 }) {
-    const canvasRef = useRef(null);
-    
-   // useEffect(() => {
-  //    draw_diagram(canvasRef.current, data);
-  //  }, [data]);
-    
-    return (
-		<canvas ref={canvasRef} className={styles.canvas} width="1200" height="600">
-			<p> There is supposed to be a diagram here, but it doesn&apos;t work on your device for some reason. </p>
-			<p> I don&apos;t take engineering, so you might have to ask me to fix it otherwide I wont notice. </p>
-	    </canvas>
-    )
+
+  const canvasRef = useRef(null);
+  
+  useEffect(() => {
+    draw_diagram(canvasRef.current!, field, current, force, charge);
+  }, [field, current, force, charge]);
+  
+  return (
+  <canvas ref={canvasRef} className={styles.canvas} width="1200" height="600">
+    <p> There is supposed to be a diagram here, but it doesn&apos;t work on your device for some reason. </p>
+    <p> I don&apos;t take engineering, so you might have to ask me to fix it otherwide I wont notice. </p>
+    </canvas>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import styles from '../../styles/page.module.css';
+import styles from './page.module.css';
 import { useState } from 'react';
 import Canvas from './canvas';
 import { direction, charge } from './helper';
@@ -45,16 +45,28 @@ function Details({
         <label htmlFor="current">Current:</label>
         <UniqueDirectionSelect id="current"
                                used_directions={used_directions}
-                               selected={field}
-                               onChange={handleFieldChange} />
+                               selected={current}
+                               onChange={handleCurrentChange} />
       </p>
       
       <p> 
         <label htmlFor="force">Force:</label>
         <UniqueDirectionSelect id="force"
                                used_directions={used_directions}
-                               selected={field}
-                               onChange={handleFieldChange} />
+                               selected={force}
+                               onChange={handleForceChange} />
+      </p>
+      <p> 
+        <label htmlFor="charge">Charge:</label>
+        <select aria-label="select a charge"
+                value={charge ?? ""}
+                onChange={e => e.target.value === "" 
+                               ? handleChargeChange(null) 
+                               : handleChargeChange(e.target.value.toLowerCase() as charge)}>
+          <option value=""></option>
+          <option value="positive">Positive</option>
+          <option value="negative">Negative</option>
+        </select>
       </p>
     </div> 
    )
@@ -71,16 +83,17 @@ function UniqueDirectionSelect({
    onChange: (x: directionOrNull) => void,
    id: string
   }) {
+  console.log(selected);
   return (
     <select aria-label="select a direction"
             value={selected ?? ""}
             onChange={e => e.target.value === "" 
                            ? onChange(null) 
-                           : onChange(e.target.value as direction)} 
+                           : onChange(e.target.value.toLowerCase() as direction)} 
             required 
             id={id}>
-      <option value=""></option>
-      <AvailableDirections used_directions={used_directions} />
+      <option value="">?</option>
+      <AvailableDirections used_directions={used_directions} selected={selected} />
     </select>
   )
 }
@@ -94,23 +107,22 @@ function capitalize(s: string) {
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
-function AvailableDirections({ used_directions }: { used_directions: (directionOrNull)[] }) {
+function AvailableDirections({ used_directions, selected }: { used_directions: (directionOrNull)[], selected: directionOrNull }) {
   const linked_directions = [ 
     create_linked_direction("left", "right"),
     create_linked_direction("up", "down"),
     create_linked_direction("out the page", "in the page")
   ]
 
-  let available = "";
+  let available: JSX.Element[] = [];
+
+  // the selected one needs to be an option.
 
   for (const ld of linked_directions) {
-    if (!(used_directions.includes(ld[0]) || used_directions.includes(ld[1]))) {
-      available += (
-        <>
-          <option key={ld[0]} value={ld[0]}>{capitalize(ld[0])}</option>
-          <option key={ld[1]} value={ld[1]}>{capitalize(ld[1])}</option>
-        </>
-      )
+    // if the direction pair has not been used already, or if the selected direction is in the direction pair
+    if (!(used_directions.includes(ld[0]) || used_directions.includes(ld[1])) || ld.includes(selected)) {
+      available.push(<option key={ld[0]} value={ld[0]}>{capitalize(ld[0])}</option>);
+      available.push(<option key={ld[1]} value={ld[1]}>{capitalize(ld[1])}</option>);
     }
   }
 
