3dc7bf79e7141132a54ae00ca43e9d1132dc4eeb
George <george200579@gmail.com>
2024-01-22 20:47:20 +0000

    basic field done

--- src/app/canvas.tsx
+++ src/app/canvas.tsx
@@ -4,7 +4,18 @@ import { direction, charge } from './helper';
 
 type directionOrNull = direction | null;
 
+function deg_to_rad(x: number) {
+	return x * (Math.PI / 180);
+}
+
 function draw_diagram(canvas:HTMLCanvasElement, field: directionOrNull, current: directionOrNull, force: directionOrNull, charge: charge | null) {
+	function draw_line(x1:number, y1:number, x2:number, y2:number) {
+		ctx.beginPath();
+		ctx.moveTo(x1, y1);
+		ctx.lineTo(x2, y2);
+		ctx.stroke();
+	}
+  
   function draw_arrow(fromx: number, fromy: number, tox: number, toy: number) {
     ctx.beginPath();
     const headlen = 8; // length of head in pixels
@@ -19,29 +30,128 @@ function draw_diagram(canvas:HTMLCanvasElement, field: directionOrNull, current:
     ctx.stroke();
   }
 
+	function draw_stroke_circle(x: number, y: number, radius: number) {
+		ctx.beginPath();
+		ctx.arc(x, y, radius, 0, 2 * Math.PI);
+		ctx.stroke();
+	}
+
+	function draw_fill_circle(x: number, y: number, radius: number)  {
+		ctx.beginPath();
+		ctx.arc(x, y, radius, 0, 2 * Math.PI);
+		ctx.fill();
+	}
+
+  function draw_x(x: number, y: number, radius: number)  {
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.rotate(deg_to_rad(45));
+    draw_line(0, -radius, 0, radius);
+    draw_line(-radius, 0, radius, 0);
+    ctx.restore();
+	}
+
   function draw_field() {
-    function draw_circles
-    switch (field) {
-      case "left":
-        console.log("unimplemenmted");
-        break;
-      case "right":
-        console.log("unimplemenmted");
-        break;
-      case "up":
-        console.log("unimplemenmted");
-        break;
-      case "down":
-        console.log("unimplemenmted");
-        break;
-      case "in the page":
-        console.log("unimplemenmted");
-        break;
-      case "out the page":
-        console.log("unimplemenmted");
-        break;
+    function draw_circle_in_page(x: number, y: number, radius: number) {
+      draw_stroke_circle(x, y, radius);
+      draw_fill_circle(x, y, radius / 5);
     }
 
+    function draw_circle_out_page(x: number, y: number, radius: number) {
+      draw_stroke_circle(x, y, radius);
+      draw_x(x, y, radius);
+    }
+
+    function draw_circles(spacing: number,
+                          radius: number,
+                          draw_circle: (x:number, y: number, radius: number) => void) {
+      ctx.save();
+      // these are just used to keep track of how much its been offset so far.
+      // don't actully use them when translating, apart from the one there.
+      const initial_offset: [number, number] = [radius + 5, radius + 5];
+
+      let total_x_offset = initial_offset[0];
+      let total_y_offset = initial_offset[1];
+      ctx.translate(...initial_offset);
+    
+      while (total_y_offset < canvas.height) {
+        // each column
+        ctx.save();
+        while (total_x_offset < canvas.width) {
+          // each row
+          draw_circle(0, 0, radius);
+          ctx.translate(spacing, 0);
+          total_x_offset += spacing;
+        }
+        ctx.restore();
+        ctx.translate(0, spacing);
+        total_x_offset = initial_offset[0];
+        total_y_offset += spacing;
+      }
+      ctx.restore();
+    }
+
+    const margin: [number, number] = [15, 15];
+
+    function draw_arrows_vertically(offset: number, reverse:boolean) {
+      ctx.save();
+      const arrow_length = canvas.height - 2 * margin[1];
+      let total_x_offset = margin[0];
+      ctx.translate(...margin);
+
+      while (total_x_offset < canvas.width - margin[0]) {
+        if (reverse) {
+          draw_arrow(0, arrow_length, 0, 0);
+        } else {
+          draw_arrow(0, 0, 0, arrow_length);
+        }
+        ctx.translate(offset, 0); 
+        total_x_offset += offset;
+      }
+    }
+
+    function draw_arrows_horizontally(offset: number, reverse: boolean) {
+      ctx.save();
+      const arrow_length = canvas.width - 2 * margin[0]
+      let total_y_offset = margin[1];
+      ctx.translate(...margin);
+    
+      while (total_y_offset < canvas.height - margin[1]) {
+        if (reverse) {
+          draw_arrow(arrow_length, 0, 0, 0);
+        } else {
+          draw_arrow(0, 0, arrow_length, 0); 
+        }
+        ctx.translate(0, offset); 
+        total_y_offset += offset;
+      }
+      ctx.restore();
+    }
+
+    ctx.save();
+    ctx.fillStyle = "blue";
+    ctx.strokeStyle = "blue";
+    switch (field) {
+      case "left":
+        draw_arrows_horizontally(100, true);
+        break;
+      case "right":
+        draw_arrows_horizontally(100, false);
+        break;
+      case "up":
+        draw_arrows_vertically(100, true);
+        break;
+      case "down":
+        draw_arrows_vertically(100, false);
+        break;
+      case "in the page":
+        draw_circles(100, 15, draw_circle_in_page);
+        break;
+      case "out the page":
+        draw_circles(100, 15, draw_circle_out_page);
+        break;
+    }
+    ctx.restore();
     return;
   }
 
@@ -54,7 +164,8 @@ function draw_diagram(canvas:HTMLCanvasElement, field: directionOrNull, current:
   }
 
   function draw_particle() {
-
+    function draw_charge
+    function draw_particle
   }
 
   // ive asserted it's not null, but i still check for it. It's just to get rid of the errors above.
@@ -70,10 +181,10 @@ function draw_diagram(canvas:HTMLCanvasElement, field: directionOrNull, current:
 	ctx.font = "20px Helvetica";
 	ctx.textBaseline = "middle";
 	ctx.textAlign = "center";
-	ctx.fillStyle = "#FFFFFF";
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = "#000000";
-    
+
+  draw_field();
 }
 
 export default function Canvas({
